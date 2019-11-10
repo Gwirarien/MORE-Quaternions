Fixed modulo calculation. Encryption/decryption still not working correctly

encryption_helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sympy as sp
 import random
 from math import sqrt
 from itertools import count, islice
@@ -20,7 +21,8 @@ class EncryptionHelper:
     @staticmethod
     def generate_random_prime_number():
         while True:
-            random_number = random.randint(50, 200)
+            # the small values are just a workaround
+            random_number = random.randint(3, 7) # TODO: find a way to support larger values
             if(EncryptionHelper.is_prime_number(random_number)):
                 return random_number
 
@@ -29,63 +31,64 @@ class EncryptionHelper:
         if m >= 0:
             return m % p
         else:
-            return -(np.abs(m) % p)
-
-    def __euclidean_algorithm(self, a, b):
-        """Extended great common divisor
-        returns (gcd(a,b), x, y) such that ax + by = gcd(a,b)"""
-        if a == 0:
-            return (b, 0, 1)
-        else:
-            g, y, x = self.__euclidean_algorithm(self.signed_modulo(b, a), a)
-            return (g, x - (b // a) * y, y)
+            return -(np.fmod(np.abs(m), p))
 
     @staticmethod
     def inverse_modulo(a, m):
-        """Inverse modulo operation"""
-        g, x, y = EncryptionHelper.__euclidean_algorithm(a, m)
-        if g != 1:
-            # If a modular inverse has not been found, break and start again with a new matrix
-            raise Exception
-        else:
-            return EncryptionHelper.signed_modulo(x, m)
-
-    @staticmethod
-    def compute_determinant(candidate_matrix):
-        a = candidate_matrix[0, 0]
-        b = candidate_matrix[0, 1]
-        c = candidate_matrix[1, 0]
-        d = candidate_matrix[1, 1]
-        return (a/100000 * d - b/100000 * c)*100000
+        a = EncryptionHelper.signed_modulo(a, m);
+        for x in range(1, m):
+            if (EncryptionHelper.signed_modulo((a*x), m) == 1):
+                return x
+        return 1 #No inverse mod
 
     @staticmethod
     def has_inverse(matrix):
-        determinant = EncryptionHelper.compute_determinant(matrix)
+        determinant = np.linalg.det(matrix)
         if determinant != 0:
             return True
 
     @staticmethod
-    def compute_inverse_matrix(determinant, matrix, mod_value):
-        """Computes the inverse of a 2x2 matrix"""
-        inv_determinant = EncryptionHelper.inverse_modulo(determinant, mod_value)
+    def matrix_minor(matrix, i, j):
+        return np.delete(np.delete(matrix, i, axis=0), j, axis=1)
 
-        inverse_matrix = np.zeros(2,2)
-        inverse_matrix[0, 0] = matrix[1, 1]
-        inverse_matrix[0, 1] = -matrix[0, 1]
-        inverse_matrix[1, 0] = -matrix[1, 0]
-        inverse_matrix[1, 1] = matrix[0, 0]
+    @staticmethod
+    def compute_modular_inverse(matrix, mod_value):
+        size = len(matrix)
+        adj = np.zeros(shape=(size, size))
+        determinant = int(round(np.linalg.det(matrix)))
+        for i in range(0, size):
+            for j in range(0, size):
+                sign = (-1)**(i+j)
+                minor = EncryptionHelper.matrix_minor(matrix, j, i)
+                adj[i][j] = (sign*int(round(np.linalg.det(minor))))%mod_value
+        return np.matrix((EncryptionHelper.inverse_modulo(determinant, mod_value) * adj)%mod_value).astype(int)
 
-        return inv_determinant * inverse_matrix
+    @staticmethod
+    def has_modular_inverse(matrix, mod_value):
+        try:
+            determinant = np.linalg.det(matrix)
+            if ((determinant != 0) and (EncryptionHelper.inverse_modulo(round(determinant), mod_value) != 1)):
+                return True #investigate if a 4x4 matrix is correctly calculated
+        except:
+            return False
+
+    @staticmethod
+    def create_modular_inverse_matrix(mod_value):
+        while True:
+            matrix = EncryptionHelper.generate_random_square_matrix(mod_value)
+            try:
+                if(EncryptionHelper.has_modular_inverse(matrix, mod_value)):
+                    return matrix
+            except:
+                pass
 
     @staticmethod
     def generate_random_square_matrix(mod_value):
-        matrix = (np.random.rand(2,2)*1000000000).astype(int)
+        matrix = (np.random.rand(2,2)*10000).astype(int)
         return matrix % mod_value
 
     @staticmethod
     def create_quaternion(sigma, mod_value):
-        alfa, beta, gamma = (EncryptionHelper.signed_modulo(random.randint(1, 100000000), mod_value) for i in range(3))
-        return Quaternion(sigma, alfa*mod_value, beta*mod_value, gamma*mod_value)
-
-
+        alfa, beta, gamma = (random.randint(1, 1000)%mod_value for i in range(3))
+        return Quaternion(sigma, alfa*mod_value, beta*mod_value, gamma*mod_value) # not sure if it should be divided again by mod_value
 

key_generator.py
@@ -20,31 +20,27 @@ class KeyGenerator:
         self.__N = p * q
         self.__N_squared = np.power(self.__N, 2)
 
-
     def __generate_sub_matrices(self):
-        k1, k2, k3, k4 = (EncryptionHelper.generate_random_square_matrix(self.__N_squared) for i in range(4))
-        for i in k1, k2, k3, k4:
-            if(EncryptionHelper.has_inverse(i)):
-                k1, i = i, k1
-                matrices = k1, k2, k3, k4
-                return matrices
-            else:
-                self.__generate_sub_matrices()
+        k1 = EncryptionHelper.create_modular_inverse_matrix(self.__N_squared)
+        k2, k3, k4 = (EncryptionHelper.generate_random_square_matrix(self.__N_squared) for i in range(3))
+        return k1, k2, k3, k4
 
     def __create_K_matrix(self):
         while True:
             sub_matrices = self.__generate_sub_matrices()
             K_matrix = np.bmat([[sub_matrices[0], sub_matrices[1]],[sub_matrices[2], sub_matrices[3]]])
-            if(np.linalg.det(K_matrix) != 0):
-                return K_matrix, sub_matrices[0]
+            if(EncryptionHelper.has_modular_inverse(K_matrix, self.__N_squared)):
+                return np.matrix(K_matrix), np.matrix(sub_matrices[0])
 
     def generate_secret_key(self):
         p, q = self.__generate_prime_numbers()
         self.__compute_modulo_number(p, q)
         K_matrix, k1_matrix = self.__create_K_matrix()
-        return (k1_matrix, np.linalg.inv(k1_matrix), K_matrix, np.linalg.inv(K_matrix))
+        k1_inverse = EncryptionHelper.compute_modular_inverse(k1_matrix, self.__N_squared)
+        K_inverse = EncryptionHelper.compute_modular_inverse(K_matrix, self.__N_squared)
+        return (k1_matrix, k1_inverse, K_matrix, K_inverse)
 
-
+# TODO: start a debug from the beginning and verify every calculation at least 5 times
 
 
 

matrix_decryption.py
@@ -9,12 +9,12 @@ class MatrixDecryption:
     def __init__(self, obj):
         # TODO: find another way to pass the __N and __N_squared args
         self.__N = obj.get_N()
-        self.__N = obj.get_N_squared()
+        self.__N_squared = obj.get_N_squared()
 
     def decrypt_message(self, secret_key, encrypted_matrix):
-        rebuilt_message_matrix = secret_key[3].dot((encrypted_matrix)/100000).dot(secret_key[2])*100000
+        rebuilt_message_matrix = (secret_key[3].dot(encrypted_matrix)).dot(secret_key[2])
         M_1 = np.array([[rebuilt_message_matrix[0,0], rebuilt_message_matrix[0,1]],[rebuilt_message_matrix[1,0], rebuilt_message_matrix[1,1]]])
-        M = secret_key[1].dot((M_1)/100000).dot(secret_key[0])*100000
+        M = secret_key[1].dot(M_1).dot(secret_key[0])
         m = M[1,1]
         m_1 = M_1[1,1]
-        print(EncryptionHelper.signed_modulo((m.real), self.__N), EncryptionHelper.signed_modulo((m_1.real), self.__N))
+        print(m.real%self.__N, m_1.real%self.__N)

matrix_encryption.py
@@ -13,21 +13,21 @@ class MatrixEncryption:
         self.__N_squared = obj.get_N_squared()
 
     def __convert_message(self, message):
-        return message % (self.__N_squared)
+        return EncryptionHelper.signed_modulo(message, self.__N_squared)
 
     def __create_first_message_matrix(self, m):
-        r1, r2 = (random.randint(1, 1000000000)%self.__N_squared for i in range(2))
+        r1, r2 = (random.randint(1, 1000)%self.__N_squared for i in range(2))
         return np.array([[m, r1],[0, r2]])
 
     def __create_second_message_matrix(self, m_1):
-        r1_1 = random.randint(1, 1000000000) % self.__N_squared
+        r1_1 = random.randint(1, 1000)%self.__N_squared
         return np.array([[m_1, r1_1], [0, 0]])
 
     def __create_message_sub_matrices(self, secret_key, message):
         m = EncryptionHelper.create_quaternion(message, self.__N)
         M = self.__create_first_message_matrix(m)
         # TODO: replace this with the method crated in matrix_operations
-        M_1 = secret_key[0].dot((M)/100000).dot(secret_key[1])*100000 # TODO: fix overflow
+        M_1 = (secret_key[0].dot(M)).dot(secret_key[1])
         m_1 = EncryptionHelper.create_quaternion(message, self.__N)
         M_2 = self.__create_second_message_matrix(m_1)
         return M_1, M_2
@@ -37,4 +37,4 @@ class MatrixEncryption:
         M_1, M_2 = self.__create_message_sub_matrices(secret_key, converted_message)
         R = EncryptionHelper.generate_random_square_matrix(self.__N_squared)
         M_final = np.bmat([[M_1, R],[np.zeros([2,2]), M_2]])
-        return secret_key[2].dot((M_final)/100000).dot(secret_key[3])*100000
+        return (secret_key[2].dot(M_final)).dot(secret_key[3])